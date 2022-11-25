1ba50605db57ec6b8e90190b6a84d8a17b26adb9
Carloshlc007 <carloshlc@hotmail.com>
2022-11-25 01:07:57 -0300

    iniciando sistema de paginaçao

--- pokedex/assets/js/poke-api.js
+++ pokedex/assets/js/poke-api.js
@@ -2,15 +2,15 @@
 
 const pokeapi = {};
 
-function convertPokeapiDetailtoPokemon(pokedetail){
+function convertPokeapiDetailtoPokemon(pokedetail) {
      const pokemon = new Pokemon()
 
-     pokemon.num = pokedetail.order
+     pokemon.num = pokedetail.id
      pokemon.name = pokedetail.name
 
      const types = pokedetail.types.map((typeSlot) => typeSlot.type.name)
      const [type] = types
-     
+
      pokemon.types = types
      pokemon.type = type
 
@@ -19,19 +19,19 @@ function convertPokeapiDetailtoPokemon(pokedetail){
      return pokemon
 }
 
-pokeapi.getPokemonsDetails = (pokenon) =>{
+pokeapi.getPokemonsDetails = (pokenon) => {
      return fetch(pokenon.url)
           .then(response => response.json())
           .then(convertPokeapiDetailtoPokemon)
 }
 
-pokeapi.getPokemons = (offset = 0, limit = 10) => {
+pokeapi.getPokemons = (offset = 0, limit = 150) => {
      const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
      return fetch(url).then(response => response.json())
-     .then(jsonBody => jsonBody.results)
-     .then(pokemons => pokemons.map(pokeapi.getPokemonsDetails))
-     .then(detailRequests => Promise.all(detailRequests))
-     .then(pokemondetails => pokemondetails)
-     .catch(error => {console.error(error)})
+          .then(jsonBody => jsonBody.results)
+          .then(pokemons => pokemons.map(pokeapi.getPokemonsDetails))
+          .then(detailRequests => Promise.all(detailRequests))
+          .then(pokemondetails => pokemondetails)
+          .catch(error => { console.error(error) })
 }
