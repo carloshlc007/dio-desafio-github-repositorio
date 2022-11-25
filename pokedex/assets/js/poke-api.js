a77d716c9eafc444721a2e961f1d361b86562398
Carloshlc007 <carloshlc@hotmail.com>
2022-11-25 01:30:09 -0300

    1

--- pokedex/assets/js/poke-api.js
+++ pokedex/assets/js/poke-api.js
@@ -25,7 +25,7 @@ pokeapi.getPokemonsDetails = (pokenon) => {
           .then(convertPokeapiDetailtoPokemon)
 }
 
-pokeapi.getPokemons = (offset = 0, limit = 150) => {
+pokeapi.getPokemons = (offset = 0, limit = 10) => {
      const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
      return fetch(url).then(response => response.json())
